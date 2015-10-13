d91d8add66c5cd776529bfb837e0553c9a7d397e
Hudolf Hess <hudolf@meuspedidos.com.br>
2015-10-13 11:12:42 -0300

    Intercom libs Base and GCM separated in different packages.

---
+++ IntercomForXamarinAndroidGCM/IntercomForXamarinAndroidGCM/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using Android.App;
+[assembly: AssemblyTitle ("IntercomForXamarinAndroidGCM")]
+[assembly: AssemblyDescription ("GCM integration for Xamarin Android.")]
+[assembly: AssemblyCopyright ("MeusPedidos")]
+[assembly: AssemblyVersion ("1.1.7")]
