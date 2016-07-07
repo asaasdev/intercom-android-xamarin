a2527ad8a81f3bd8b056cf592f2d6eae1a188fea
Hudolf Hess <hudolf@meuspedidos.com.br>
2016-07-07 17:39:17 -0300

    Atualizado sdk (versão 1.1.18)

--- IntercomForXamarinAndroidGCM/IntercomForXamarinAndroidGCM/Properties/AssemblyInfo.cs
+++ IntercomForXamarinAndroidGCM/IntercomForXamarinAndroidGCM/Properties/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using Android.App;
 [assembly: AssemblyTitle ("IntercomForXamarinAndroidGCM")]
 [assembly: AssemblyDescription ("GCM integration for Xamarin Android.")]
 [assembly: AssemblyCopyright ("MeusPedidos")]
-[assembly: AssemblyVersion ("1.1.7")]
+[assembly: AssemblyVersion ("1.1.18")]
