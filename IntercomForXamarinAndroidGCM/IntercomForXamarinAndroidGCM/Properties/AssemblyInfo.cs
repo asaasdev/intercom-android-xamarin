fb71eb3e5732067f9b92e384bb50260b4f102f8f
Matheus Souza <matheus.souza@asaas.com.br>
2017-08-04 14:23:54 -0300

    Atualizado versão

--- IntercomForXamarinAndroidGCM/IntercomForXamarinAndroidGCM/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using Android.App;
-[assembly: AssemblyTitle ("IntercomForXamarinAndroidGCM")]
-[assembly: AssemblyDescription ("GCM integration for Xamarin Android.")]
-[assembly: AssemblyCopyright ("MeusPedidos")]
-[assembly: AssemblyVersion ("1.1.18")]
