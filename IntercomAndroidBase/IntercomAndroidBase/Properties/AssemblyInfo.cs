dafcff20493e119c64521028f740e1ca07ab0bbd
Matheus Souza <matheus.souza@asaas.com.br>
2018-01-03 11:41:36 -0200

    Fazendo atualizacao do Intercom para a ultima versao

--- IntercomAndroidBase/IntercomAndroidBase/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using Android.App;
-[assembly: AssemblyTitle ("IntercomForXamarinAndroidBase")]
-[assembly: AssemblyDescription ("Integrate Intercom into your Android Xamarin application.")]
-[assembly: AssemblyCopyright ("MeusPedidos")]
-[assembly: AssemblyVersion ("1.1.18")]
